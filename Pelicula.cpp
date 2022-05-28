768101d4ffe52b89dea04124f28eb6065b50fd81
Christopher Pedraza Pohlenz <62347713+christopher-pedraza@users.noreply.github.com>
2022-05-28 10:44:26 -0500

    Agregar clase de objeto Pelicula

---
+++ Pelicula.cpp
@@ -0,0 +1,5 @@
+#include "Pelicula.hpp"
+
+Pelicula::Pelicula(int _id, int _duracion, int _calificacion, string _genero, string _nombre) : Video(_id, _duracion, _calificacion, _genero, _nombre) {
+    // Mismos atributos que objeto video
+}
