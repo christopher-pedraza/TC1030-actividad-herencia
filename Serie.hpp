2b33e6530a12c8fe60d7e93698e52cec22ec101a
Christopher Pedraza Pohlenz <62347713+christopher-pedraza@users.noreply.github.com>
2022-05-28 11:04:50 -0500

    Permitir agregar hasta 10 episodios por serie

--- Serie.hpp
+++ Serie.hpp
@@ -7,22 +7,26 @@ class Serie : public Video {
     private:
         // Atributos
         int cantidad_episodios, temporada;
-        string titulo_Ep;
+        Video episodios[10];
 
     public:
         // Metodos
         // Constructor
-        Serie(int _cantidad_episodios, int _temporada, string _titulo_Ep, int _id, int _duracion, int _calificacion, string _genero, string _nombre);
+        Serie(int _cantidad_episodios, int _temporada, int _id, int _duracion, int _calificacion, string _genero, string _nombre);
 
         // Setters
         void setEpisodios(int _cantidad_episodios);
         void setTemporada(int _temporada);
-        void setTituloEp(string _titulo_Ep, int _index);
+        void setTituloEp(Video _titulo_Ep, int _index);
 
         // Getters
         int getCantidadEpisodios();
         int getTemporada();
-        string getTituloEp(int _index);
+        Video getEpisodio(int _index);
+
+        // Adiocionales
+        void agregarEpisodio(Video _episodio);
+        void agregarEpisodio(int _id, int _duracion, int _calificacion, string _genero, string _nombre);
 };
 
 #endif /* Serie_hpp */
