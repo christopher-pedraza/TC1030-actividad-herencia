b8374d1e6b0d3a6e62252d312605ccd00c96223d
Christopher Pedraza Pohlenz <62347713+christopher-pedraza@users.noreply.github.com>
2022-06-14 12:58:52 -0500

    Agregar método para mostrar videos con base a una calificacion

--- Codigo/Pelicula.cpp
+++ Codigo/Pelicula.cpp
@@ -3,3 +3,7 @@
 Pelicula::Pelicula(int _id, int _duracion, int _calificacion, std::string _genero, std::string _nombre) : Video(_id, _duracion, _calificacion, _genero, _nombre) {
     // Mismos atributos que objeto video
 }
+
+void Pelicula::imprimir() {
+    
+}
